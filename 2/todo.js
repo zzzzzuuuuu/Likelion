06f6dcb28c087e65d880abcd84b1339fc824bec3
zzzzzuuuuu <agu0830@naver.com>
2023-04-07 02:15:16 +0900

    feat: todo.js 코드 수정, 오류 해결

--- 2/todo.js
+++ 2/todo.js
@@ -31,8 +31,7 @@ let id = 0;
  * @param {'create' | 'read' | 'update' | 'delete'} command
  * @param {{ id: number, title: string, text: string } | null} value
  * */
-const todoControl = (command, value) => { //todoControl: 커멘드와 값을 입력 받아 알맞은 함수를 호출
-  // 대충 뭘 작성함
+const todoControl = (command, value) => {
   switch (command) {
     case 'create': createTodo(value); 
       break;
@@ -40,23 +39,23 @@ const todoControl = (command, value) => { //todoControl: 커멘드와 값을 입
       break;
     case 'update': updateTodo(value); 
       break;
-    case 'delete': deleteTodo(id);
+    case 'delete': deleteTodo(value);
       break;
     default: console.log('command error!');
       break;
   }
 };
 
-// 보너스: newTodo를 비구조화 할당하여 todos에 추가하세요. 비구조화할당 = 구조분해할당
+// 보너스: newTodo를 비구조화 할당하여 todos에 추가하세요.
 /** @param {{ id: number, title: string, text: string }} newTodo */
-const createTodo = (newTodo) => { // todo 정보를 받아 추가
-  // todos에 새로운 todo를 삽입
+const createTodo = (newTodo) => {
   const {id, title, text} = newTodo;
+  todos.push({id, title, text}); // 값을 push해서 넣어주기!!! 윗줄을 사용해야함
 };
 
 // 출력 형식: 'id: 0, ttile: 타이틀, text: 텍스트'
 // 보너스: 리터럴 문자를 활용해 출력하세요.
-const readTodo = () => { // 전체 목록을 하나씩 읽음
+const readTodo = () => {
   for (const i of todos) { // todos 배열에서 값 하나하나 꺼내오기
     console.log(`id: ${i.id}, title: ${i.title}, text: ${i.text}`); // 출력
   }
@@ -65,41 +64,24 @@ const readTodo = () => { // 전체 목록을 하나씩 읽음
 // 에러처리: 만약 업데이트하려는 todo를 찾을 수 없는 경우 'id = (n)에 맞는 todo를 찾을 수 없습니다.' 를 출력하세요.
 // 보너스: if문을 쓰지 말고 업데이트 하세요. 단, 에러 처리에는 if문을 써도 됩니다. (힌트: 논리연산자)
 /** @param {{ id: number, title: string, text: string }} targetTodo */
-const updateTodo = (targetTodo) => { // todo 정보(id포함)를 받아 해당 todo를 수정 push?
-  // if (업데이트 하고자 하는 todo를 찾을 수 없다면) {
-  //   return 에러 메시지를 출력하세요 }
-  // for(const i=0; i<todos.length; i++) { 
-  //   if (!id.includes(todos[i].id)) { // id 값이 데이터에 존재하는지 확인
-  //     return console.log(`id = ${id}에 맞는 todo를 찾을 수 없습니다.`);
-  //   }
-  // }
-  if (todos.filter((i) => i.id != targetTodo.id)) {
-    return console.log(`id = ${id}에 맞는 todo를 찾을 수 없습니다.`);
+const updateTodo = (targetTodo) => {
+  if (!(todos.filter((i) => i.id === targetTodo.id))) { // targetTodo의 id값이 존재하는지 확인함
+    return console.log(`id = ${targetTodo.id}에 맞는 todo를 찾을 수 없습니다.`); // 존재하지 않으면 해당 id에 맞는 todo를 찾을 수 없다고 출력
   }
   // 업데이트 로직
-  for (const i=0; i<todos.length; i++) {
-    todos[i] = (i.id === targetTodo.id ? targetTodo : i); // 조건문 ? true : false 
-  }
+  todos = todos.map((i) => (i.id === targetTodo.id ? targetTodo : i));
+
 };
 
 // 에러처리: 만약 업데이트하려는 todo를 찾을 수 없는 경우 'id = n에 맞는 todo를 찾을 수 없습니다.' 를 출력하세요.
 // 보너스: filter 함수를 사용해 구현해보세요
 /** @param {number} id */
-const deleteTodo = (id) => { // id를 받아 해당 todo 삭제 delete
-  // if (해당하는 todo id가 todo 데이터에 없다면) {
-  //   return console.log(여기에 에러 메시지를 쓰세요); }
-
-  // for(const i=0; i<todos.length; i++) { 
-  //   if (!id.includes(todos[i].id)) {
-  //     return console.log(`id = ${id}에 맞는 todo를 찾을 수 없습니다.`);
-  //   }
-  // }
-
-  if (todos.filter((i) => i.id != id)) {
-    return console.log(`id = ${id}에 맞는 todo를 찾을 수 없습니다.`);
+const deleteTodo = (id) => {
+  if (!(todos.filter((i) => i.id === id))) { // targetTodo의 id값이 존재하는지 확인
+    return console.log(`id = ${id}에 맞는 todo를 찾을 수 없습니다.`); // 존재하지 않을 때 출력
   }
   // 삭제 로직
-  todos = todos.filter((i) => i.id !== id); // filter() 함수는 해당 모든 배열을 저장 
+  todos = todos.filter((i) => i.id !== id);
 };
 
 /*
@@ -121,30 +103,30 @@ const deleteTodo = (id) => { // id를 받아 해당 todo 삭제 delete
 const test = () => {
   todoControl('bug!!!');
   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((idx) =>
-    todoControl('create', {
-      id: id++,
-      title: `title${idx}`,
-      text: `text${idx}`,
-    })
+      todoControl('create', {
+          id: id++,
+          title: `title${idx}`,
+          text: `text${idx}`,
+      })
   );
   [1, 2, 10].map((id) => todoControl('delete', id));
   [3, 4, 100].map((id) =>
-    todoControl('update', { id: id, title: `타이틀${id}`, text: `텍스트${id}` })
+      todoControl('update', { id: id, title: `타이틀${id}`, text: `텍스트${id}` })
   );
   todoControl('read');
 
   const testValue = [0, 3, 4, 5, 6, 7, 8, 9].map((id) => {
-    if (id === 3 || id === 4) {
-      return { id: id, title: `타이틀${id}`, text: `텍스트${id}` };
-    }
-    return { id: id, title: `title${id}`, text: `text${id}` };
+      if (id === 3 || id === 4) {
+          return { id: id, title: `타이틀${id}`, text: `텍스트${id}` };
+      }
+      return { id: id, title: `title${id}`, text: `text${id}` };
   });
 
   // true가 출력되어야합니다.
   console.log(
-    JSON.stringify(testValue) === JSON.stringify(todos)
-      ? '통과!!'
-      : '실패!! 다시 풀어보세요'
+      JSON.stringify(testValue) === JSON.stringify(todos)
+          ? '통과!!'
+          : '실패!! 다시 풀어보세요'
   );
 };
 
